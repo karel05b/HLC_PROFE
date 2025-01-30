8fee3f8dc9b4336aa517e750ecc1709e67f3bf47
Angela Ríder <aridjim249@g.educaand.es>
2025-01-30 12:49:51 +0100

    Clase 30 enero

--- leerFiltro.php
+++ leerFiltro.php
@@ -1,6 +1,23 @@
 <?php
 // Incluir el archivo de conexión (si es necesario)
 include('conexion.php');
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de Estudiantes filtrado por nombre</title>
+    
+    <!-- Link al CSS de Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-4">
+        <h2 class="text-center mb-4">Listado de Estudiantes Filtrado</h2>
+
+        
+<?php
 
 // Verificar si se ha enviado el formulario por POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -12,8 +29,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $resultado = mysqli_query($conexion, $query);
 
         // Verificar si la consulta fue exitosa
-        if (!$resultado) {
-            die("Error en la consulta: " . mysqli_error($conexion));
+        if (mysqli_num_rows($resultado) == 0) {
+            //pngo un mensaje para explicar que no se ha encontrado
+            //un alumno con ese nombre.
+
+            //Le facilito un enlace al fichero de alta de alumnos, por si quiere introducirlo.
+
+            // Le puedo facilitar un enlace al fichero que muestra todos los alumnos.
+
+            // Añado un boton de volver para, que vuelva a la pagina de opciones.php.
+            ?>
+            <div class="card-body">
+                <div class="mb-3">
+                    <a href="#.php" class="btn btn-primary">Introducir alumnos</a>
+                </div>
+
+                <div class="mb-3">
+                    <a href="leerTodos.php" class="btn btn-primary">Ver alumnos</a>
+                </div>
+
+                <div class="mb-3">
+                    <a href="opciones.php" class="btn btn-primary">Volver</a>
+                </div>
+            </div>
+            <?php
         }
 
         // Mostrar los resultados en formato de tabla
@@ -55,3 +94,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 ?>
+<!-- Scripts de Bootstrap -->
+     <!-- Agregar el script de Bootstrap 5 desde el CDN al final del body -->
+     <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
