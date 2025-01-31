9b12bae37a0e464521ff9a3862beb19aa19cf40b
Angela Ríder <aridjim249@g.educaand.es>
2025-01-31 08:28:26 +0100

    plantilla

---
+++ introducirDatos.php
@@ -0,0 +1,21 @@
+<?php
+// Incluir el archivo de conexión (si es necesario)
+include('conexion.php');
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de Estudiantes filtrado por nombre</title>
+    
+    <!-- Link al CSS de Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+?>
+<!-- Scripts de Bootstrap -->
+     <!-- Agregar el script de Bootstrap 5 desde el CDN al final del body -->
+     <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
